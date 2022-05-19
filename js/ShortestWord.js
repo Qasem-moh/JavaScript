6e87e222a0883ee7e56f4e22bbbd82a275232370
lap <2171246jo@s.aou.edu.jo>
2022-05-19 16:06:06 -0700

    js

--- js/ShortestWord.js
+++ js/ShortestWord.js
@@ -8,9 +8,57 @@ let stringWord="bitcoin take over the world maybe who knows perhaps"
 // }
 //
 // console.log(findShort(stringWord))
-
+/////////////////////////////////////////////////////////////
 function findShort(s){
     return Math.min.apply(null, s.split(' ').map(w => w.length));
 }
 
 console.log(findShort(stringWord))
+/////////////////////////////////////////////////
+// function findShort(s){
+//     return Math.min(...s.split(" ").map (s => s.length));
+// }
+//////////////////////////////////////////////////////////
+
+// function findShort(s){
+//     var arr = s.split(' ');
+//     var smallest = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if(arr[i].length < smallest.length){
+//             smallest = arr[i];
+//         }
+//     }
+//     return smallest.length;
+// }
+///////////////////////////////////////////////////////
+
+// const findShort = (s) => s
+//     .split(' ')
+//     .sort((a, b) => b.length - a.length)
+//     .pop()
+//     .length
+/////////////////////////////////////////////////
+
+// function findShort(s) {
+//     return s.split(' ').reduce((min, word) => Math.min(min, word.length), Infinity);
+// }
+//////////////////////////////////////////////
+
+// function findShort(s){
+//     var arr = s.split(' ');
+//     var min = arr[0].length;
+//     for (var i = 1; i < arr.length; i++) {
+//         if (arr[i].length < min) {
+//             min = arr[i].length;
+//         }
+//     }
+//     return min
+// }
+///////////////////////////////////////////////
+function findShort(s){
+    return s
+        .split(' ')
+        .map(a => a.length)
+        .reduce((a, b) => Math.min(a, b))
+}
+
