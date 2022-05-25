bafc3c9c3d50c72f49b18dbc4ecdfede28a573e3
lap <2171246jo@s.aou.edu.jo>
2022-05-25 02:39:24 -0700

    js

---
+++ js/challange78.js
@@ -0,0 +1,30 @@
+/*
+  Higher Order Functions Challenges
+
+  You Can Use
+  - ,
+  - _
+  - Space
+  - True => 1 => One Time Only In The Code
+
+  You Cannot Use
+  - Numbers
+  - Letters
+
+  - You Must Use [Filter + Map + Reduce + Your Knowledge]
+  - Order Is Not Important
+  - All In One Chain
+
+*/
+
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let solution = myString.split("").filter(function (ele, ind, arr) {
+    return isNaN(parseInt(ele)) && ele !== "," && !ele.includes(arr[arr.length - true]);
+}).map(function (ele) {
+    return ele === "_" ? " " : ele;
+}).reduce(function (acc, current) {
+    return acc === current ? acc : acc + current;
+})
+
+console.log(solution); // Elzero Web School
