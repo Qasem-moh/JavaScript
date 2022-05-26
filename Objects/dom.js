b77a9cd0d346f09b524bef89dfb9e8ae0972f721
lap <2171246jo@s.aou.edu.jo>
2022-05-26 00:58:30 -0700

    js

---
+++ Objects/dom.js
@@ -0,0 +1,15 @@
+/**
+  DOM
+  - What Is DOM
+  - DOM Selectors
+  --- Find Element By ID
+  --- Find Element By Tag Name
+  --- Find Element By Class Name
+  --- Find Element By CSS Selectors
+  --- Find Element By Collection
+  ------ title
+  ------ body
+  ------ images
+  ------ forms
+  ------ links
+*/
